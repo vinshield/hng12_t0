84a4e6fc3981bbc6850838e0a0fd42828a63eca6
Jesse <jeslelacodes@gmail.com>
2025-01-31 17:10:45 +0100

    Add error handling and 404 response for undefined routes

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+app.use((req, res) => {
+	res.status(404).json({ error: "Route not found" });
+});
+
 app.get("/", (req, res) => {
-	res.json({
-		email: "jeslelacodes@gmail.com",
-		current_datetime: new Date().toISOString(),
-		github_url: "https://github.com/vinshield/hng12_t0",
-	});
+	try {
+		res.status(200).json({
+			email: "jeslelacodes@gmail.com",
+			current_datetime: new Date().toISOString(),
+			github_url: "https://github.com/vinshield/hng12_t0",
+		});
+	} catch (error) {
+		res.status(500).json({ error: "internal server error" });
+	}
 });
 const port = process.env.PORT || 2000;
 
